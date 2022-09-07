67fa0c2ebfa35deab6ca0d231ceca2d23a8faaea
Joe Vieira <joe.vieira@integritas.net>
2022-09-06 22:59:37 -0700

    Adding CSVPrinter class that implements a write method

---
+++ CSVPrinter.java
@@ -0,0 +1,13 @@
+public class PrintToCSV {
+    private String data[];
+
+    public PrintToCSV(String[] data) {
+        this.data = data;
+    }
+
+    public String replaceSpecialChars(String data) {
+        return data;
+    }
+
+    
+}
